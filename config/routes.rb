11d94569d01a946cb9d06f19bdae94b00bf02110
ericmtodd <eterictodd@gmail.com>
2021-12-13 20:00:40 -0700

    Switched back to custom routes utilizing local variable passing for each form.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   root "kittens#index"
 
   # Kittens routes
-  # get "kittens", to: "kittens#index"
-  # get "kittens/new", to: "kittens#new", as: "new_kitten"
-  # post "kittens", to: "kittens#create", as: "create_kitten"
-  # get "kittens/:id", to: "kittens#show", as: "show_kitten"
-  # delete "kittens/:id", to: "kittens#destroy", as: "destroy_kitten"
-  # get "kittens/:id/edit", to: "kittens#edit", as: "edit_kitten"
-  # patch "kittens/:id", to: "kittens#update", as: "update_kitten"
-
-  resources :kittens
+  get "kittens", to: "kittens#index"
+  get "kittens/new", to: "kittens#new", as: "new_kitten"
+  post "kittens", to: "kittens#create", as: "create_kitten"
+  get "kittens/:id", to: "kittens#show", as: "show_kitten"
+  delete "kittens/:id", to: "kittens#destroy", as: "destroy_kitten"
+  get "kittens/:id/edit", to: "kittens#edit", as: "edit_kitten"
+  patch "kittens/:id", to: "kittens#update", as: "update_kitten"
 end
